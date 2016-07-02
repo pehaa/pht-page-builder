666e0c38236a479ed348f71dab975d3746fa26cc
Paulina Hetman <info@pehaa.com>
2016-07-02 15:26:41 +0200

    partial display fixed

--- admin/partials/phtpb-gmaps-options-section-display.php
+++ admin/partials/phtpb-gmaps-options-section-display.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Provide a admin area view for the plugin
  *
@@ -8,8 +7,8 @@
  * @link       https://github.com/pehaa/pht-page-builder
  * @since      2.9.0
  *
- * @package    PeHaaThemes_Simple_Post_Types
- * @subpackage PeHaaThemes_Simple_Post_Types/admin/partials
+ * @package    PeHaa_Themes_Page_Builder
+ * @subpackage PeHaa_Themes_Page_Builder/admin/partials
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -17,6 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }?>
 
 <p id="phtpb-gmaps-auth">
-	<?php esc_html_e( 'From June 22, 2016 the Google Maps Javascript API no longer supports keyless access (any request that doesn\'t include an API key). Don’t be afraid, getting an API key is really fast and simple.', $this->name ); ?>
-	<a href="https://developers.google.com/maps/documentation/javascript/get-api-key"><?php esc_html_e( 'Get a Key/Authentication', $this->name ); ?></a>
+	<?php esc_html_e( 'From June 22, 2016 the Google Maps Javascript API no longer supports keyless access (any request that doesn\'t include an API key). Don’t be afraid, getting an API key is really fast and simple.', 'phtpb' ); ?>
+	<a href="https://developers.google.com/maps/documentation/javascript/get-api-key"><?php esc_html_e( 'Get a Key/Authentication', 'phtpb' ); ?></a>
 </p>
