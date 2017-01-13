9248bcac48d025c02c12f22eb9692c36bdb8fe2a
PeHaa <info@pehaa.com>
2017-01-13 17:06:40 +0100

    preparing update

--- includes/class-phtpb.php
+++ includes/class-phtpb.php
@@ -77,7 +77,7 @@ class PeHaa_Themes_Page_Builder {
 		}
 
 		$this->plugin_name = 'phtpb';
-		$this->version = '3.1.0';
+		$this->version = '3.1.1';
 
 		$this->load_dependencies();
 		$this->set_locale();	
