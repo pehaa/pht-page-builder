55b0a97d48c30f35c5b4789877089572c4affeb6
Paulina Hetman <info@pehaa.com>
2017-08-24 09:02:22 +0200

    version 3.3.3

--- includes/class-phtpb.php
+++ includes/class-phtpb.php
@@ -77,7 +77,7 @@ class PeHaa_Themes_Page_Builder {
 		}
 
 		$this->plugin_name = 'phtpb';
-		$this->version = '3.3.2';
+		$this->version = '3.3.3';
 
 		$this->load_dependencies();
 		$this->set_locale();	
